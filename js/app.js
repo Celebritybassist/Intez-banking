53aef93aaeafcf8876307737e70925f4a4ce4744
ranken.dev <oshiobugieagwegiokhe@gmail.com>
2023-04-07 11:03:59 +0100

    t

--- js/app.js
+++ js/app.js
@@ -10,15 +10,3 @@ class User{
     }
     
 }
-class User{
-    constructor(){}
-
-    static login(username, password){
-        window.sessionStorage.setItem(username, password);
-    }
-
-    static logout(){
-         window.sessionStorage.clear();
-    }
-    
-}
